2f4259454328d8becc2b41c8d59f6415ab793127
Conan Yuan <conanyuan@gmail.com>
2014-02-02 23:22:33 -0500

    move extend into a base class

--- bridge.js
+++ bridge.js
@@ -1,29 +1,24 @@
-var bridge = bridge || {};
 
 // Following recommendations in
 // http://www.adobe.com/devnet/html5/articles/javascript-object-creation.html
 
-// Constants
-
-bridge.consts = {
-    SUITS: ["CLUBS", "DIAMONDS", "HEARTS", "SPADES"],
-    RANKS: ["A", 2, 3, 4, 5, 6, 7, 8, 9, "T", "J", "Q", "K"],
-    POINTS: {"A": 4, "K": 3, "Q": 2, "J": 1},
-};
+var bridge = bridge || {};
 
 /**************************************************************************
- * Card
+ * Infrastructure
  **************************************************************************/
 
-bridge.card = {
-    // Members
-    // id, suit_id, and rank_id are all 1-indexed
-    id      : null,
-    suit    : null,
-    suit_id : null,
-    rank    : null,
-    rank_id : null,
+(function() {
+    if (typeof Object.create !== "function") {
+        Object.create = function (o) {
+            function F() {}
+            F.prototype = o;
+            return new F();
+        };
+    }
+}());
 
+bridge._object = {
     extend: function(props) {
         var prop, obj;
         obj = Object.create(this);
@@ -34,6 +29,62 @@ bridge.card = {
         }
         return obj;
     },
+}
+
+bridge._ordered_dict = bridge._object.extend({
+    values : [],
+    name : null,
+    is_valid_value: function(value) {
+	if (value < 1 || value > this.values.length) {
+	    throw "Invalid " + this.name + " id: " + value;
+	}
+	return true;
+    }
+    is_valid_name: function(name) {
+	if (this.values.indexOf(name) < 0) {
+	    throw "Invalid " + this.name + ": " + name;
+	}
+	return true;
+    }
+    from_value: function(value) {
+	this.is_valid_value(value);
+	return this.values[value-1];
+    }
+    from_name: function(name) {
+	this.is_valid_name(name);
+	return this.indexOf(name)+1;
+    }
+    length: function() {
+	return this.values.length;
+    }
+    get: function(idx) {
+	return this.values[idx];
+    }
+});
+
+/**************************************************************************
+ * Constants
+ **************************************************************************/
+
+bridge.consts = {
+    SUITS:   ["CLUBS", "DIAMONDS", "HEARTS", "SPADES"],
+    STRAINS: ["CLUBS", "DIAMONDS", "HEARTS", "SPADES", "NO_TRUMP"],
+    RANKS:   ["A", 2, 3, 4, 5, 6, 7, 8, 9, "T", "J", "Q", "K"],
+    POINTS:  {"A": 4, "K": 3, "Q": 2, "J": 1},
+};
+
+/**************************************************************************
+ * Card
+ **************************************************************************/
+
+bridge.card = bridge._object.extend({
+    // Members
+    // id, suit_id, and rank_id are all 1-indexed
+    id      : null,
+    suit    : null,
+    suit_id : null,
+    rank    : null,
+    rank_id : null,
 
     init: function() {
 	if (this.id !== null &&
@@ -107,8 +158,11 @@ bridge.card = {
     to_string: function() {
 	return this.rank + this.suit.substring(0, 1);
     },
-};
+});
 
+/**************************************************************************
+ * Deck
+ **************************************************************************/
 // Note that bridge.card.id is 1-indexed, but bridge.deck, like any
 // array, is 0-indexed
 bridge.deck = [];
@@ -120,20 +174,9 @@ for (var ii = 0; ii < bridge.consts.SUITS.length * bridge.consts.RANKS.length; i
  * Hand
  **************************************************************************/
 
-bridge.hand = {
+bridge.hand = bridge._object.extend({
     cards : null,
     by_suit : null,
-
-    extend: function(props) {
-        var prop, obj;
-        obj = Object.create(this);
-        for(prop in props) {
-            if(props.hasOwnProperty(prop)) {
-                obj[prop] = props[prop];
-            }
-        }
-        return obj;
-    },
 
     init: function() {
 	for (var ii = 0; ii < this.cards.length; ii++) {
@@ -179,10 +222,39 @@ bridge.hand = {
     to_string: function() {
 	return this.cards.map(function(c) {return c.to_string()}).join(", ");
     }
-};
+});
 
 bridge.handrange = {
 };
+
+bridge.bid = {
+    // rank + strain
+};
+
+/**************************************************************************
+ * Test
+ **************************************************************************/
+
+bridge.strategy = {};
+
+// Given a bid history, and a hand, figure out what the next bid
+// should be.
+bridge.strategy.make_bid = function(bid_history, hand) {
+};
+
+// Given a bid history (and a hand), return possible hand ranges for
+// the last bid.
+bridge.strategy.interpret_bid = function(bid_history, hand) {
+};
+
+/*
+  Main functions:
+
+  if opening (your partner hasn't bid):
+    - if 16-18 points + balanced, bid 1NT
+    - if 
+
+ */
 
 /**************************************************************************
  * Test
@@ -214,25 +286,5 @@ bridge.test = function() {
     return h;
 };
 
-/*
-  Main functions:
-  1. Given a bid history, and a hand, figure out what the next bid should be.
-
-  if opening (your partner hasn't bid):
-    - if 16-18 points + balanced, bid 1NT
-    - if 
-
-  2. Given a bid history (and a hand), return possible hand ranges for all other players.
- */
-
-(function() {
-    if (typeof Object.create !== "function") {
-        Object.create = function (o) {
-            function F() {}
-            F.prototype = o;
-            return new F();
-        };
-    }
-}());
-
+/**************************************************************************/
 
