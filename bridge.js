48c07ed0157f32d14053d5d806b671972341c257
Conan Yuan <conanyuan@gmail.com>
2014-02-02 22:24:29 -0500

    add bridge.deck and flesh out bridge.hand

--- bridge.js
+++ bridge.js
@@ -4,10 +4,12 @@ var bridge = bridge || {};
 // http://www.adobe.com/devnet/html5/articles/javascript-object-creation.html
 
 // Constants
+
 bridge.consts = {
     SUITS: ["CLUBS", "DIAMONDS", "HEARTS", "SPADES"],
     RANKS: ["A", 2, 3, 4, 5, 6, 7, 8, 9, "T", "J", "Q", "K"],
-}
+    POINTS: {"A": 4, "K": 3, "Q": 2, "J": 1},
+};
 
 /**************************************************************************
  * Card
@@ -15,6 +17,7 @@ bridge.consts = {
 
 bridge.card = {
     // Members
+    // id, suit_id, and rank_id are all 1-indexed
     id      : null,
     suit    : null,
     suit_id : null,
@@ -35,7 +38,7 @@ bridge.card = {
     init: function() {
 	if (this.id !== null &&
 	    (this.id < 1 ||
-	    this.id > bridge.consts.SUITS.length * bridge.consts.N_CARDS_PER_SUIT)) {
+	    this.id > bridge.consts.SUITS.length * bridge.consts.RANKS.length)) {
 	    throw "Invalid card id: " + this.id;
 	}
 
@@ -86,13 +89,24 @@ bridge.card = {
 		this.suit = bridge.consts.SUITS[this.suit_id-1];
 	    }
             if (this.rank === null) {
-		this.rank_id = this.id % bridge.consts.RANKS.length + 1;
+		this.rank_id = (this.id-1) % bridge.consts.RANKS.length + 1;
 		this.rank = bridge.consts.RANKS[this.rank_id-1];
 	    }
 	}
 	return this;
     },
+
+    to_string: function() {
+	return this.rank + this.suit.substring(0, 1);
+    },
 };
+
+// Note that bridge.card.id is 1-indexed, but bridge.deck, like any
+// array, is 0-indexed
+bridge.deck = [];
+for (var ii = 0; ii < bridge.consts.SUITS.length * bridge.consts.RANKS.length; ii++) {
+    bridge.deck.push(bridge.card.extend({ id: ii+1 }).init());
+}
 
 /**************************************************************************
  * Hand
@@ -114,12 +128,51 @@ bridge.hand = {
     },
 
     init: function() {
+	for (var ii = 0; ii < this.cards.length; ii++) {
+	    if (typeof this.cards[ii] === 'number') {
+		this.cards[ii] = bridge.deck[this.cards[ii]];
+	    }
+	}
 	this.by_suit = {};
-	
+	for (var ii = 0; ii < bridge.consts.SUITS.length; ii++) {
+	    this.by_suit[bridge.consts.SUITS[ii]] = [];
+	}
+	this.cards.sort(function(a,b) {return a.id - b.id});
+	for (var ii = 0; ii < this.cards.length; ii++) {
+	    this.by_suit[this.cards[ii].suit].push(this.cards[ii]);
+	}
 	return this;
-    }
+    },
 
-    
+    hc_points: function() {
+	var points = 0;
+	for (var ii = 0; ii < this.cards.length; ii++) {
+	    if (this.cards[ii].rank in bridge.consts.POINTS) {
+		points += bridge.consts.POINTS[this.cards[ii].rank];
+	    }
+	}
+	return points;
+    },
+
+    is_balanced: function() {
+	var seen_doubleton = false;
+	for (var suit in this.by_suit) {
+	    if (this.by_suit[suit].length < 2) {
+		return false;
+	    } else if (this.by_suit[suit].length == 2) {
+		if (seen_doubleton) {
+		    return false;
+		} else {
+		    seen_doubleton = true;
+		}
+	    }
+	}
+	return true;
+    },
+
+    to_string: function() {
+	return this.cards.map(function(c) {return c.to_string()}).join(", ");
+    }
 };
 
 bridge.handrange = {
@@ -132,11 +185,24 @@ bridge.handrange = {
 bridge.test = function() {
     c = bridge.card.extend({
 	id: 4,
-	}).init();
+    }).init();
     if (c.suit != "CLUBS") {
 	throw "Bad suit";
     }
-    return c;
+    c = bridge.deck[4];
+    if (c.suit != "CLUBS") {
+	throw "Bad suit";
+    }
+    h = bridge.hand.extend({
+	cards: [0, 1, 2, 13, 14, 15, 26, 27, 28, 39, 40, 41],
+    }).init();
+    if (h.is_balanced() != true) {
+	throw "Unbalanced";
+    }
+    if (h.hc_points() != 16) {
+	throw "Wrong points";
+    }
+    return h;
 };
 
 /*
